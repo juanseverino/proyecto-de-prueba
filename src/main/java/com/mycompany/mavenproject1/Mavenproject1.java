e1cfd2949a25fb9c9f2c7f8ca5bcd747ceb08eb4
Lab5 <Lab5@LAB4-PC12>
2024-10-18 20:39:26 -0300

    subida inicial

---
+++ src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.mavenproject1;
+
+import java.util.ArrayList; 
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+       ArrayList<Integer> ci=new ArrayList<Integer>();
+       ci.add(44455566);
+       System.out.println("CEDULA DE IDENTIDAD"+ci.get(0));
+    }
+}
