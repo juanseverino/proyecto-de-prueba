e691226a6b2fdabf7f5fffdb243b052935f0f2bb
Lab5 <Lab5@LAB4-PC12>
2024-10-18 20:59:10 -0300

    agregamos un for

--- src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
+++ src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -1,14 +1,24 @@
 
 package com.mycompany.mavenproject1;
 
-import java.util.ArrayList; 
+import java.util.*; 
 public class Mavenproject1 {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
         
        ArrayList<Integer> ci=new ArrayList<Integer>();
-       ci.add(44455566);
+       ci.add( 44455566);
        System.out.println("CEDULA DE IDENTIDAD"+ci.get(0));
+       Scanner tec=new Scanner(System.in);
+       int cedula;
+       
+       for(int i=0, i<5, i++);
+       cedula=tec.nextInt();
+       ci.add(cedula);
+       System.out.println("CEDULA DE IDENTIDAD"+ci.get(0));
+       
+       
+       
     }
 }
